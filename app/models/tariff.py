ac1d953fe1a78e26e95866da01e670d7059b10d4
Dmitry Abramov <abramovdv42@gmail.com>
2023-12-11 22:04:40 +0700

    relationship user tariff

--- app/models/tariff.py
+++ app/models/tariff.py
@@ -1,11 +1,17 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import Column, String, Text
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, relationship
 
 from app.core.db import Base
 from app.core.config import settings
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Tariff(Base):
     name: Mapped[str] = Column(String(length=settings.max_length_string),
                                unique=True, nullable=False)
     description: Mapped[str] = Column(Text)
+    users: Mapped[list['User']] = relationship(back_populates='users')
--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,14 @@
 import enum
+from typing import TYPE_CHECKING
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
-from sqlalchemy import Enum, Column, String
-from sqlalchemy.orm import Mapped
+from sqlalchemy import Enum, Column, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.core.db import Base
+
+if TYPE_CHECKING:
+    from .tariff import Tariff
 
 
 class UserRoleEnum(enum.Enum):
@@ -18,3 +22,7 @@ class User(SQLAlchemyBaseUserTable[int], Base):
         Enum(UserRoleEnum), default=UserRoleEnum.user, nullable=False
     )
     username: Mapped[str] = Column(String(length=100), nullable=False)
+    tariff_id: Mapped[int] = Column(
+        ForeignKey('tariff.id'),
+    )
+    tariff: Mapped['Tariff'] = relationship(back_populates='users')
