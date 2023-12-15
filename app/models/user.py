d324309c35867fc3555d1f49070e99a3d9505a53
Dmitry Abramov <abramovdv42@gmail.com>
2023-12-15 11:01:26 +0700

    Сделал модель Notification и промежуточную notification_user_association
    Переделал миграции

--- app/models/user.py
+++ app/models/user.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import enum
 from typing import TYPE_CHECKING
 
@@ -7,11 +9,13 @@ from sqlalchemy.orm import Mapped, relationship
 
 from app.core.db import Base
 from .group import group_user_association
+from .notification import notification_user_association
 
 if TYPE_CHECKING:
     from .tariff import Tariff
     from .profile import Profile
     from .group import Group
+    from .notification import Notification
 
 
 class UserRoleEnum(enum.Enum):
@@ -32,3 +36,7 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     profile: Mapped['Profile'] = relationship(back_populates='user')
     groups: Mapped['Group'] = relationship(secondary=group_user_association,
                                            back_populates='users')
+    notifications: Mapped[Notification] = relationship(
+        secondary=notification_user_association,
+        back_populates='users'
+    )
