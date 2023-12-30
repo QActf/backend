825cd20e38d311ecd9c1f781ce888965c8f0d935
Dmitry Abramov <abramovdv42@gmail.com>
2023-12-30 07:02:11 +0700

    add paginator

--- app/api/endpoints/profile.py
+++ app/api/endpoints/profile.py
@@ -1,6 +1,9 @@
 import re
 
-from fastapi import APIRouter, Depends, UploadFile, File, status, HTTPException
+from fastapi import (
+    APIRouter, Depends, UploadFile, File, status, HTTPException, Query,
+    Response
+)
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import User, Profile
@@ -13,12 +16,30 @@ from app.services.utils import create_filename, save_content, remove_content
 router = APIRouter()
 
 
+def get_pagination_params(
+    # offset must be greater than or equal to 0
+    offset: int = Query(0, ge=0),
+    # limit must be greater than 0
+    limit: int = Query(10, gt=0)
+):
+    return {"offset": offset, "limit": limit}
+
+
 @router.get('/', response_model=list[ProfileRead],
             dependencies=[Depends(current_superuser)])
 async def get_all_profiles(
-    session: AsyncSession = Depends(get_async_session)
+    response: Response,
+    session: AsyncSession = Depends(get_async_session),
+    pagination: dict = Depends(get_pagination_params)
 ):
-    return await profile_crud.get_multi(session)
+    offset = pagination['offset']
+    limit = pagination['limit']
+    end = offset + limit
+    profiles = await profile_crud.get_multi(session)
+    response.headers['X-Total-Count'] = str(len(profiles))
+    response.headers['X-Offset'] = str(offset)
+    response.headers['X-Limit'] = str(limit)
+    return profiles[offset:end]
 
 
 @router.get('/me', response_model=ProfileRead)
