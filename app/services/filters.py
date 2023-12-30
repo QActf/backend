f30815d3708e64774b3cd99b7acbdca8906d233b
Dmitry Abramov <abramovdv42@gmail.com>
2023-12-31 01:28:57 +0700

    добавил поиск по имени фамилии

--- app/services/filters.py
+++ app/services/filters.py
@@ -9,6 +9,9 @@ from app.models import Profile
 class ProfileFilter(Filter):
     age__gte: Optional[int] = Field(None)
     age__lte: Optional[int] = Field(None)
+    first_name__ilike: Optional[str] = Field(None)
+    last_name__ilike: Optional[str] = Field(None)
 
     class Constants(Filter.Constants):
         model = Profile
+        search_model_fields = ['first_name', 'last_name']
