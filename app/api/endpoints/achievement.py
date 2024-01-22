9e675912882837d66bcaeee136e6b42d62d64100
Sovraska <simon.novikov@gmail.com>
2024-01-23 01:23:05 +0300

    rewrite error handle logic

--- app/api/endpoints/achievement.py
+++ app/api/endpoints/achievement.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.validators import check_name_duplicate
+from app.api.validators import check_name_duplicate, check_obj_exists
 from app.core.db import get_async_session
 from app.core.user import current_superuser, current_user
 from app.crud import achievement_crud
 from app.schemas.achievement import (AchievementCreate, AchievementRead,
                                      AchievementUpdate)
-from app.services.endpoints_services import delete_obj, get_obj
+from app.services.endpoints_services import delete_obj
 
 router = APIRouter()
 
@@ -34,9 +34,8 @@ async def get_achievement(
         session: AsyncSession = Depends(get_async_session),
 ) -> AchievementRead:
     """Возвращает achievement."""
-    return await get_obj(
-        obj_id=achievement_id, crud=achievement_crud, session=session
-    )
+    await check_obj_exists(achievement_id, achievement_crud, session)
+    return await achievement_crud.obj(obj_id=achievement_id, session=session)
 
 
 @router.post(
@@ -63,8 +62,9 @@ async def update_achievement(
         achievement: AchievementUpdate,
         session: AsyncSession = Depends(get_async_session)
 ):
-    _achievement = await get_obj(
-        obj_id=achievement_id, crud=achievement_crud, session=session
+    await check_obj_exists(achievement_id, achievement_crud, session)
+    _achievement = await achievement_crud.obj(
+        obj_id=achievement_id, session=session
     )
     return await achievement_crud.update(_achievement, achievement, session)
 
