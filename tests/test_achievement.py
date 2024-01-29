b504d47d1958098ec6625d7041ed0b6019187a1a
Dmitry Abramov <abramovdv42@gmail.com>
2024-01-29 17:07:42 +0700

    тест запрета создания ачивмент юзером

--- tests/test_achievement.py
+++ tests/test_achievement.py
@@ -49,3 +49,21 @@ class TestCreateAchievement:
         new_achievements = await db_session.execute(stmt)
         new_achievements = new_achievements.scalar()
         assert new_achievements == achievements
+
+    async def test_forbidden_create_achievemrnt_user(
+            self,
+            db_session: AsyncSession,
+            auth_client: TestClient
+    ):
+        """Тест запрета создания ачивмент юзером."""
+        stmt = func.count(Achievement.id)
+        achievements = await db_session.execute(stmt)
+        achievements = achievements.scalar()
+        response = auth_client.post(
+            '/achievements',
+            json=CREATE_SCHEME
+        )
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        new_achievements = await db_session.execute(stmt)
+        new_achievements = new_achievements.scalar()
+        assert new_achievements == achievements
