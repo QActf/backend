fd61fa5711f60b91991467360ac41500716e3678
Dmitry Abramov <abramovdv42@gmail.com>
2024-01-31 05:56:49 +0700

    refactoring

--- tests/test_achievement.py
+++ tests/test_achievement.py
@@ -1,10 +1,12 @@
 from fastapi import status
 from fastapi.testclient import TestClient
-from sqlalchemy import func, select
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from app.models import Achievement, Profile, User
+
+from .utils import get_obj_count
 
 CREATE_SCHEME = {
     'name': 'Achievment name',
@@ -18,6 +20,50 @@ UPDATE_SCHEME = {
 }
 
 
+async def _get_achievement_by_id(
+        index: int,
+        db_session: AsyncSession
+):
+    """Возвращает ачивмент по id."""
+    stmt = (
+        select(Achievement)
+        .where(Achievement.id == index)
+        .options(
+            selectinload(Achievement.profiles)
+        )
+    )
+    achievement = await db_session.execute(stmt)
+    return achievement.scalar()
+
+
+async def _get_user_by_id(
+        index: int,
+        db_session: AsyncSession
+):
+    """Возвращает юзера по id."""
+    user = await db_session.execute(
+        select(User)
+        .where(User.id == index)
+        .options(
+            selectinload(User.profile)
+            .selectinload(Profile.achievements)
+        )
+    )
+    return user.scalar()
+
+
+async def _get_profile_by_user_id(
+        index: int,
+        db_session: AsyncSession
+):
+    """Возвращает профиль юзера."""
+    profile = await db_session.execute(
+        select(Profile)
+        .where(Profile.user_id == index)
+    )
+    return profile.scalar()
+
+
 class TestCreateAchievement:
     async def test_create_achievement_superuser(
             self,
@@ -25,16 +71,13 @@ class TestCreateAchievement:
             auth_superuser: TestClient
     ):
         """Тест создания ачивмент."""
-        stmt = func.count(Achievement.id)
-        achievements = await db_session.execute(stmt)
-        achievements = achievements.scalar()
+        achievements = await get_obj_count(Achievement, db_session)
         response = auth_superuser.post(
             '/achievements',
             json=CREATE_SCHEME
         )
         assert response.status_code == status.HTTP_201_CREATED
-        new_achievements = await db_session.execute(stmt)
-        new_achievements = new_achievements.scalar()
+        new_achievements = await get_obj_count(Achievement, db_session)
         assert new_achievements == achievements + 1
 
     async def test_wrong_create_scheme(
@@ -42,16 +85,13 @@ class TestCreateAchievement:
             db_session: AsyncSession,
             auth_superuser: TestClient
     ):
-        stmt = func.count(Achievement.id)
-        achievements = await db_session.execute(stmt)
-        achievements = achievements.scalar()
+        achievements = await get_obj_count(Achievement, db_session)
         response = auth_superuser.post(
             '/achievements',
             json=WRONG_CREATE_SCHEME
         )
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-        new_achievements = await db_session.execute(stmt)
-        new_achievements = new_achievements.scalar()
+        new_achievements = await get_obj_count(Achievement, db_session)
         assert new_achievements == achievements
 
     async def test_forbidden_create_achievement_user(
@@ -60,16 +100,13 @@ class TestCreateAchievement:
             auth_client: TestClient
     ):
         """Тест запрета создания ачивмент юзером."""
-        stmt = func.count(Achievement.id)
-        achievements = await db_session.execute(stmt)
-        achievements = achievements.scalar()
+        achievements = await get_obj_count(Achievement, db_session)
         response = auth_client.post(
             '/achievements',
             json=CREATE_SCHEME
         )
         assert response.status_code == status.HTTP_403_FORBIDDEN
-        new_achievements = await db_session.execute(stmt)
-        new_achievements = new_achievements.scalar()
+        new_achievements = await get_obj_count(Achievement, db_session)
         assert new_achievements == achievements
 
     async def test_forbidden_create_achievement_nonauth(
@@ -78,16 +115,13 @@ class TestCreateAchievement:
             new_client: TestClient
     ):
         """Тест запрета создания ачивмент юзером."""
-        stmt = func.count(Achievement.id)
-        achievements = await db_session.execute(stmt)
-        achievements = achievements.scalar()
+        achievements = await get_obj_count(Achievement, db_session)
         response = new_client.post(
             '/achievements',
             json=CREATE_SCHEME
         )
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
-        new_achievements = await db_session.execute(stmt)
-        new_achievements = new_achievements.scalar()
+        new_achievements = await get_obj_count(Achievement, db_session)
         assert new_achievements == achievements
 
 
@@ -99,9 +133,7 @@ class TestGetAchievement:
             auth_superuser: TestClient
     ):
         """Тест получения всех ачивмент суперюзером."""
-        stmt = func.count(Achievement.id)
-        achievements = await db_session.execute(stmt)
-        achievements = achievements.scalar()
+        achievements = await get_obj_count(Achievement, db_session)
         response = auth_superuser.get('/achievements')
         assert response.status_code == status.HTTP_200_OK
         assert len(response.json()) == achievements
@@ -134,36 +166,10 @@ class TestGetAchievement:
             auth_client: TestClient
     ):
         """Тест получения юзером своих ачивментс."""
-        user = await db_session.execute(
-            select(User)
-            .where(User.username == register_client.username)
-            .options(
-                selectinload(User.profile)
-                .selectinload(Profile.achievements)
-            )
-        )
-        user = user.scalars().first()
-        profile = await db_session.execute(
-            select(Profile)
-            .where(Profile.user_id == user.id)
-        )
-        profile = profile.scalars().first()
-        achievement = await db_session.execute(
-            select(Achievement)
-            .where(Achievement.id == 1)
-            .options(
-                selectinload(Achievement.profiles)
-            )
-        )
-        achievement = achievement.scalars().first()
-        achiv = await db_session.execute(
-            select(Achievement)
-            .where(Achievement.id == 2)
-            .options(
-                selectinload(Achievement.profiles)
-            )
-        )
-        achiv = achiv.scalars().first()
+        user = await _get_user_by_id(register_client.id, db_session)
+        profile = await _get_profile_by_user_id(user.id, db_session)
+        achievement = await _get_achievement_by_id(1, db_session)
+        achiv = await _get_achievement_by_id(2, db_session)
         achiv.profiles.append(profile)
         achievement.profiles.append(profile)
         await db_session.commit()
@@ -184,36 +190,10 @@ class TestGetAchievement:
             auth_client: TestClient
     ):
         """Тест получения юзером своего ачивмент по id."""
-        user = await db_session.execute(
-            select(User)
-            .where(User.username == register_client.username)
-            .options(
-                selectinload(User.profile)
-                .selectinload(Profile.achievements)
-            )
-        )
-        user = user.scalars().first()
-        profile = await db_session.execute(
-            select(Profile)
-            .where(Profile.user_id == user.id)
-        )
-        profile = profile.scalars().first()
-        achievement = await db_session.execute(
-            select(Achievement)
-            .where(Achievement.id == 1)
-            .options(
-                selectinload(Achievement.profiles)
-            )
-        )
-        achievement = achievement.scalars().first()
-        achiv = await db_session.execute(
-            select(Achievement)
-            .where(Achievement.id == 2)
-            .options(
-                selectinload(Achievement.profiles)
-            )
-        )
-        achiv = achiv.scalars().first()
+        user = await _get_user_by_id(register_client.id, db_session)
+        profile = await _get_profile_by_user_id(user.id, db_session)
+        achievement = await _get_achievement_by_id(1, db_session)
+        achiv = await _get_achievement_by_id(2, db_session)
         achiv.profiles.append(profile)
         achievement.profiles.append(profile)
         await db_session.commit()
@@ -258,22 +238,14 @@ class TestUpdateAchievement:
             auth_superuser: TestClient
     ):
         """Тест апдейта ачивмент суперюзером."""
-        achievement = await db_session.execute(
-            select(Achievement)
-            .where(Achievement.id == 1)
-        )
-        achievement = achievement.scalar()
+        achievement = await _get_achievement_by_id(1, db_session)
         response = auth_superuser.patch(
             '/achievements/1',
             json=UPDATE_SCHEME
         )
         assert response.status_code == status.HTTP_200_OK
         assert response.json()['name'] == UPDATE_SCHEME['name']
-        check_achievement = await db_session.execute(
-            select(Achievement)
-            .where(Achievement.id == 1)
-        )
-        check_achievement = check_achievement.scalar()
+        check_achievement = await _get_achievement_by_id(1, db_session)
         assert check_achievement.name == UPDATE_SCHEME['name']
         assert check_achievement.description == achievement.description
 
@@ -286,17 +258,11 @@ class TestDeleteAchievement:
             auth_client: TestClient
     ):
         """Тест запрета удаления ачивмент юзером."""
-        achievements = await db_session.execute(
-            func.count(Achievement.id)
-        )
-        achievements = achievements.scalar()
+        achievements = await get_obj_count(Achievement, db_session)
         assert achievements > 0
         response = auth_client.delete('/achievements/1')
         assert response.status_code == status.HTTP_403_FORBIDDEN
-        check_achiv = await db_session.execute(
-            func.count(Achievement.id)
-        )
-        check_achiv = check_achiv.scalar()
+        check_achiv = await get_obj_count(Achievement, db_session)
         assert check_achiv == achievements
 
     async def test_forbidden_delete_acievement_nonauth(
@@ -306,17 +272,11 @@ class TestDeleteAchievement:
             new_client: TestClient
     ):
         """Тест запрета удаления ачивмент неавторизованным."""
-        achievements = await db_session.execute(
-            func.count(Achievement.id)
-        )
-        achievements = achievements.scalar()
+        achievements = await get_obj_count(Achievement, db_session)
         assert achievements > 0
         response = new_client.delete('/achievements/1')
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
-        check_achiv = await db_session.execute(
-            func.count(Achievement.id)
-        )
-        check_achiv = check_achiv.scalar()
+        check_achiv = await get_obj_count(Achievement, db_session)
         assert check_achiv == achievements
 
     async def test_delete_achievement_superuser(
@@ -326,25 +286,12 @@ class TestDeleteAchievement:
             auth_superuser: TestClient
     ):
         """Тест удаления ачивмент суперюзером."""
-        achievement = await db_session.execute(
-            select(Achievement)
-            .where(Achievement.id == 1)
-        )
-        achievement = achievement.scalar()
-        achiv_count = await db_session.execute(
-            func.count(Achievement.id)
-        )
-        achiv_count = achiv_count.scalar()
+        achievement = await _get_achievement_by_id(1, db_session)
+        assert achievement is not None
+        achiv_count = await get_obj_count(Achievement, db_session)
         response = auth_superuser.delete('/achievements/1')
         assert response.status_code == status.HTTP_204_NO_CONTENT
-        removed_achiv = await db_session.execute(
-            select(Achievement)
-            .where(Achievement.id == 1)
-        )
-        removed_achiv = removed_achiv.scalar()
+        removed_achiv = await _get_achievement_by_id(1, db_session)
         assert removed_achiv is None
-        check_achiv_count = await db_session.execute(
-            func.count(Achievement.id)
-        )
-        check_achiv_count = check_achiv_count.scalar()
+        check_achiv_count = await get_obj_count(Achievement, db_session)
         assert check_achiv_count == achiv_count - 1
