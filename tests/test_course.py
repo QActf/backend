0085337ea574a2ef8580fb24a4979df983f4d66c
Dmitry Abramov <abramovdv42@gmail.com>
2024-02-01 00:28:12 +0700

    endpoints, tests for course
    * В ручку пост добавлен статус код 201.
    * Добавлены тесты ручки пост.

---
+++ tests/test_course.py
@@ -0,0 +1,55 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import Course
+
+from .utils import get_obj_count
+
+
+CREATE_SCHEME = {
+    'name': 'Course name',
+    'description': 'Course description'
+}
+
+
+class TestCreateCourse:
+    async def test_create_course_forbidden_nonauth(
+        self,
+        new_client: TestClient
+    ):
+        """Тест запрета создания курса неавторизованным."""
+        response = new_client.post('/courses', json=CREATE_SCHEME)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    async def test_create_course_forbidden_user(
+        self,
+        auth_client: TestClient
+    ):
+        """Тест запрета создания курса юзером."""
+        response = auth_client.post('/courses', json=CREATE_SCHEME)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    async def test_create_course_superuser(
+        self,
+        db_session: AsyncSession,
+        auth_superuser: TestClient
+    ):
+        """Тест создания курса."""
+        courses_count = await get_obj_count(Course, db_session)
+        response = auth_superuser.post('/courses', json=CREATE_SCHEME)
+        assert response.status_code == status.HTTP_201_CREATED
+        check_courses_count = await get_obj_count(Course, db_session)
+        assert check_courses_count == courses_count + 1
+
+
+class TestGetCourse:
+    ...
+
+
+class TestUpdateCourse:
+    ...
+
+
+class TestDeleteCourse:
+    ...
