7d7f3c19fd64293ed2eb534bf80754d7725817d9
MikhailDevW <wildmv@gmail.com>
2024-04-23 21:51:36 +0300

    [BCK-29] добавлена документация по достижениям.

--- app/api_docs_responses/achievement.py
+++ app/api_docs_responses/achievement.py
@@ -70,3 +70,64 @@ DELETE_ACHIEVEMENT = {
     **get_204_docs(),
     **get_401_docs()
 }
+
+GET_ALL_ACHIEVEMENTS_DESCRIPTION = """
+Получение всех достижений, которые есть в БД.\n
+    Args:\n
+        None\n
+    Returns:\n
+        list: dict(Информация о достижении).\n
+    Permissions:\n
+        Доступно только суперпользователю.
+"""
+
+GET_ACHIEVEMENTS_CURRENTUSER_DESCRIPTION = """
+Получение достижений текущего пользователя.\n
+    Args:\n
+        None\n
+    Returns:\n
+        list: dict(Информация о достижении).\n
+    Permissions:\n
+        Доступно только текущему пользователю.
+"""
+
+GET_ACHIEVEMENT_CURRENTUSER_ID_DESCRIPTION = """
+Получение описания достижения пользователя по id.\n
+    Args:\n
+        achievement_id\n
+    Returns:\n
+        dict: Информация о запрашиваемом достижении.\n
+    Permissions:\n
+        Доступно только текущему пользователю.
+"""
+
+CREATE_ACHIEVEMENT_DESCRIPTION = """
+Cоздание нового достижения,
+которое может быть у любого пользователя.\n
+    Args:\n
+        None\n
+    Returns:\n
+        dict: Информация о созданно достижении.\n
+    Permissions:\n
+        Доступно только суперпользователю.
+"""
+
+PATCH_ACHIEVEMENT_ID_DESCRIPTION = """
+Редактирование существующего достижения в БД.\n
+    Args:\n
+        achievement_id\n
+    Returns:\n
+        dict: Информация о достижении после редактирования.\n
+    Permissions:\n
+        Доступно только суперпользователю.
+"""
+
+DELETE_ACHIEVEMENT_DESCRIPTION = """
+Удаление существующего достижения в БД.\n
+    Args:\n
+        achievement_id\n
+    Returns:\n
+        None\n
+    Permissions:\n
+        Доступно только суперпользователю.
+"""
