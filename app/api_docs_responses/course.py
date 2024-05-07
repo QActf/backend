556406c98fe4978fb5de19b1f139d7fe0131f1d4
NECROshizo <necroshizo@gmail.com>
2024-05-07 22:24:39 +0300

    close BCK-26 add examination docs and minor fix course

--- app/api_docs_responses/course.py
+++ app/api_docs_responses/course.py
@@ -62,15 +62,17 @@ content_course = {
 }
 
 get_courses_response = get_200_docs(content_courses)
+
 get_user_courses_response = {
     **get_200_docs(content_courses),
     **get_401_docs(),
 }
+
 get_user_course_response = {
     **get_200_docs(content_course),
     **get_401_docs(),
     **get_403_docs("Вы не записаны на данный курс."),
-    **get_404_docs("Объект course не найден.."),
+    **get_404_docs("Объект course не найден."),
 }
 
 get_course_response = {
