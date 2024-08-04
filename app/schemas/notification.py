6d3aa25295ac216ca9356f24964029be7638c76b
Leonid Tsybulsky <axonyx@mail.ru>
2024-08-04 20:50:03 +0300

    Бэк для оповещений. (#101)

--- app/schemas/notification.py
+++ app/schemas/notification.py
@@ -1,17 +1,29 @@
-from typing import Optional
+from datetime import datetime
 
 from pydantic import BaseModel
 
 
 class NotificationCreate(BaseModel):
     name: str
-    description: Optional[str]
+    message: str
 
 
 class NotificationRead(BaseModel):
     id: int
-    name: Optional[str]
-    description: Optional[str]
+    name: str
+    message: str
+
+
+class AddNotificationForUser(BaseModel):
+    user_id: int
+    notification_id: int
+
+
+class ReadNotificationForUser(BaseModel):
+    id: int
+    name: str
+    message: str = None
+    date: datetime
 
     class Config:
         from_attributes = True
