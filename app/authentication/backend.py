6020599010e19ec604ee7ed71edd91ed82274136
mxnoob <noobmx@gmail.com>
2024-08-14 17:31:50 +0300

    add logger login/logout

---
+++ app/authentication/backend.py
@@ -0,0 +1,23 @@
+import logging
+
+from fastapi import Response
+from fastapi_users import models
+from fastapi_users.authentication import backend, Strategy
+
+logger = logging.getLogger(__name__)
+
+
+class AuthenticationBackendWithLogger(backend.AuthenticationBackend):
+    async def login(
+        self, strategy: Strategy[models.UP, models.ID], user: models.UP
+    ) -> Response:
+        response = await super().login(strategy, user)
+        logger.info(f"Logged in by `{user.email}`.")
+        return response
+
+    async def logout(
+        self, strategy: Strategy[models.UP, models.ID], user: models.UP, token: str
+    ) -> Response:
+        response = await super().logout(strategy, user, token)
+        logger.info(f"Logged out by `{user.email}`.")
+        return response
