25fef2c8e1e96a4cbee6cedc71752b5ca6fa5a7d
D-Abramoc <109978091+D-Abramoc@users.noreply.github.com>
2024-09-10 18:38:54 +0700

    Добавлен блок контактной информации. (#99)
    * Добавлен бло контактной информации.
    
    * Добавлена ручка создания контактной информации
    * Переписан метод create в CRUDContactInformationBlock.
      Запись контактов возможна только один раз. Далее надо использовать метод PATCH
    * add handlers: get, patch
    
    * fix requirements, pre-commit
    
    * Update routers.py
    
    * Update requirements.txt
    
    ---------
    
    Co-authored-by: Dmitry Abramov <abramovdv42@gmail.com>
    Co-authored-by: Leonid Tsybulsky <axonyx@mail.ru>

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import (
-    achievement_router, course_router, examination_router, group_router,
-    locale_router, notification_router, profile_router, question_router,
-    subscription_router, tariff_router, task_router, user_router,
+    achievement_router, contact_info_router, course_router, examination_router,
+    group_router, locale_router, notification_router, profile_router,
+    question_router, subscription_router, tariff_router, task_router,
+    user_router,
 )
 
 main_router = APIRouter()
@@ -33,4 +34,7 @@ main_router.include_router(
 )
 main_router.include_router(
     subscription_router, prefix='/subscriptions', tags=['Subscriptions']
+)
+main_router.include_router(
+    contact_info_router, prefix='/contact_info', tags=['Contact info']
 )
